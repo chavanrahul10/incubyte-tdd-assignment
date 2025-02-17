ba994a49893c0ab12f43d49fb2e8588d9dc0c390
Rahul <chavan.rahul10@gmail.com>
2025-02-17 19:46:45 +0530

    Initial commit

---
+++ tests/stringCalculator.test.ts
@@ -0,0 +1,12 @@
+import { add } from "../src/stringCalculator";
+
+describe("String Calculator", () => {
+  it("should return 0 for an empty string", () => {
+    const result = add("");
+    expect(result).toBe(0);
+  });
+  it("should return same number for single number string", () => {
+    const result = add("1");
+    expect(result).toBe(1);
+  });
+});
