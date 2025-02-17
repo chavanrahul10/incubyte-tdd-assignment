1783697bedb682531433e21c19b44273c36e3f74
Rahul <chavan.rahul10@gmail.com>
2025-02-17 20:14:58 +0530

    Added support for custom delimiter

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -3,7 +3,13 @@ export function add(numbers: string): number {
   if (numbers.trim() === "") {
     return 0;
   }
+  let customDelimiter: string | undefined = undefined;
+  if (numbers.startsWith("//")) {
+    const delimiterEndIndex = numbers.indexOf("\n");
+    customDelimiter = numbers.slice(2, delimiterEndIndex);
+    numbers = numbers.slice(delimiterEndIndex + 1);
+  }
 
-  const numArray = numbers.split(DELIMITER);
+  const numArray = numbers.split(customDelimiter || DELIMITER);
   return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
