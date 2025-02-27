bdcc172b35c5a2de404007335b38ac04e5faa7ec
Rahul <chavan.rahul10@gmail.com>
2025-02-27 18:40:42 +0530

    - Refactored add function for custom delimiters

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -3,15 +3,12 @@ export function add(numbers: string): number {
   if (numbers.trim() === "") {
     return 0;
   }
-  let customDelimiter: string | undefined = undefined;
-  if (numbers.startsWith("//")) {
-    const delimiterEndIndex = numbers.indexOf("\n");
-    customDelimiter = numbers.slice(2, delimiterEndIndex);
-    // To support custom delimiter of any length
-    // It should be wrapped in square brackets
-    if (customDelimiter.startsWith("[") && customDelimiter.endsWith("]"))
-      customDelimiter = customDelimiter.slice(1, customDelimiter.length - 1);
-    numbers = numbers.slice(delimiterEndIndex + 1);
+  const customDelimiter = getCustomDelimiter(numbers);
+
+  if (customDelimiter) {
+    numbers = numbers.slice(
+      (customDelimiter.length === 1 ? 3 : customDelimiter.length + 4) + 1
+    );
   }
 
   const numArray = numbers.split(customDelimiter || DELIMITER).map((num) => {
@@ -30,4 +27,15 @@ function checkForNegativeNumbers(numArray: number[]) {
   if (negatives.length > 0) {
     throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
   }
+}
+
+function getCustomDelimiter(numbers: string) {
+  let customDelimiter: string | undefined = undefined;
+  if (numbers.startsWith("//")) {
+    const delimiterEndIndex = numbers.indexOf("\n");
+    customDelimiter = numbers.slice(2, delimiterEndIndex);
+    if (customDelimiter.startsWith("[") && customDelimiter.endsWith("]"))
+      customDelimiter = customDelimiter.slice(1, customDelimiter.length - 1);
+  }
+  return customDelimiter;
 }
